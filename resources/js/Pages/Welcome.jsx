237bd6086df96f1a9da4e196fb8153cffe93d1d3
udara amarasekara <udaraprabath98@gmail.com>
2025-06-09 16:00:52 +0530

    course cards finished on homepage

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,17 +1,6 @@
 import { Head, Link } from "@inertiajs/react";
-
+import { StarIcon } from "@heroicons/react/24/solid";
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
-    const handleImageError = () => {
-        document
-            .getElementById("screenshot-container")
-            ?.classList.add("!hidden");
-        document.getElementById("docs-card")?.classList.add("!row-span-1");
-        document
-            .getElementById("docs-card-content")
-            ?.classList.add("!flex-row");
-        document.getElementById("background")?.classList.add("!hidden");
-    };
-
     return (
         <>
             <Head title="Welcome" />
@@ -42,61 +31,111 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                 />
                             </div>
                             <div className="flex flex-col">
-                              <div className="rounded-full hidden md:block my-7 relative w-48 h-48 bg-[#60A5FA]">
-                                <img
-                                    className="absolute left-7 bottom-10 object-cover   w-auto h-[90%]"
-                                    src="image 8.png "
-                                />
+                                <div className="rounded-full hidden md:block my-7 relative w-48 h-48 bg-[#60A5FA]">
+                                    <img
+                                        className="absolute left-7 bottom-10 object-cover   w-auto h-[90%]"
+                                        src="image 8.png "
+                                    />
+                                </div>
+                                <div className="rounded-full  my-7 relative w-48 h-48 bg-[#FACC15]">
+                                    <img
+                                        className="absolute right-15 bottom-12 object-cover   w-auto h-[90%]"
+                                        src="image 7.png "
+                                    />
+                                </div>
                             </div>
-                            <div className="rounded-full  my-7 relative w-48 h-48 bg-[#FACC15]">
-                                <img
-                                    className="absolute right-15 bottom-12 object-cover   w-auto h-[90%]"
-                                    src="image 7.png "
-                                />
-                            </div>
- 
-                           </div>
-
                         </div>
                     </div>
                 </div>
                 <div className="w-full p-4 flex flex-col md:flex-row bg-[#F8FAFC] ">
-                  <div className="flex md:border-r-4 border-[#E2E8F0] flex-col w-full md:w-1/4 items-center justify-center">
-                    <div className="m-4 font-bold md:text-5xl" >250+</div>
-                    <div className="text-lg">Courses by our best mentors</div>
-                  </div>  
-                   <div className="flex md:border-r-4 border-[#E2E8F0] flex-col w-full md:w-1/4 items-center justify-center">
-                    <div className="m-4 font-bold md:text-5xl" >250+</div>
-                    <div className="text-lg">Courses by our best mentors</div>
-                  </div> 
-                   <div className="flex md:border-r-4 border-[#E2E8F0] flex-col w-full md:w-1/4 items-center justify-center">
-                    <div className="m-4 font-bold md:text-5xl" >250+</div>
-                    <div className="text-lg">Courses by our best mentors</div>
-                  </div> 
-                   <div className="flex flex-col md:w-1/4 w-full items-center justify-center">
-                    <div className="m-4 font-bold md:text-5xl" >250+</div>
-                    <div className="text-lg">Courses by our best mentors</div>
-                  </div> 
+                    <div className="flex md:border-r-4 border-[#E2E8F0] flex-col w-full md:w-1/4 items-center justify-center">
+                        <div className="m-4 font-bold md:text-5xl">250+</div>
+                        <div className="text-lg">
+                            Courses by our best mentors
+                        </div>
+                    </div>
+                    <div className="flex md:border-r-4 border-[#E2E8F0] flex-col w-full md:w-1/4 items-center justify-center">
+                        <div className="m-4 font-bold md:text-5xl">250+</div>
+                        <div className="text-lg">
+                            Courses by our best mentors
+                        </div>
+                    </div>
+                    <div className="flex md:border-r-4 border-[#E2E8F0] flex-col w-full md:w-1/4 items-center justify-center">
+                        <div className="m-4 font-bold md:text-5xl">250+</div>
+                        <div className="text-lg">
+                            Courses by our best mentors
+                        </div>
+                    </div>
+                    <div className="flex flex-col md:w-1/4 w-full items-center justify-center">
+                        <div className="m-4 font-bold md:text-5xl">250+</div>
+                        <div className="text-lg">
+                            Courses by our best mentors
+                        </div>
+                    </div>
                 </div>
             </div>
-             <div className="bg-white  md:h-screen flex flex-col">
-              <div className="flex flex-row p-8 justify-between">
-                <div className="text-xl font-bold" >Top Categories</div>
-                <div className="text-lg text-[#3B82F6]">See all</div>
-              </div> 
-
-                <div className="flex flex-row gap-3 justify-between p-8">
-                                        {[...Array(5)].map((_, i) => (
-
-                   <div className="w-full md:w-1/6 rounded-xl border aspect-square items-center justify-center flex flex-col  border-[#E2E8F0]">
-                      <div className="w-24 h-24 flex items-center justify-center text-3xl font-extrabold text-[#3B82F6] rounded-full bg-[#E0F2FE]" >d</div>
-                      <div className="text-2xl font-bold">Subject</div>
-                      <div className="text-[#334155] text-lg">No of Courses</div>
-                   </div>
-                                         ))}
-
+            <div className="bg-white  md:h-screen flex flex-col">
+                <div className="flex flex-row p-8 justify-between">
+                    <div className="text-xl font-bold">Top Categories</div>
+                    <div className="text-lg text-[#3B82F6]">See all</div>
                 </div>
 
+                <div className="flex flex-col md:flex-row gap-3 justify-between p-8">
+                    {[...Array(5)].map((_, i) => (
+                        <div
+                            key={i}
+                            className="w-full md:w-1/6 rounded-xl border aspect-square items-center justify-center flex flex-col  border-[#E2E8F0]"
+                        >
+                            <div className="w-24 h-24 flex items-center justify-center text-3xl font-extrabold text-[#3B82F6] rounded-full bg-[#E0F2FE]">
+                                d
+                            </div>
+                            <div className="text-2xl font-bold">Subject</div>
+                            <div className="text-[#334155] text-lg">
+                                No of Courses
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className="flex flex-row p-8 justify-between">
+                    <div className="text-xl font-bold">Top Courses</div>
+                    <div className="text-lg text-[#3B82F6]">See all</div>
+                </div>
+
+                <div className="flex flex-col md:flex-row gap-3 justify-between p-8">
+                    {[...Array(5)].map((_, i) => (
+                        <div
+                            key={i}
+                            className="w-full md:w-1/6 rounded-xl border aspect-square items-start justify-between flex flex-col  border-[#E2E8F0]"
+                        >
+                            <img
+                                className="object-cover w-full rounded-t-xl  h-48"
+                                src="Rectangle 1080.png"
+                            />
+                            <div className="pl-5 mt-3">
+                                <div className="text-2xl font-bold">
+                                    Course Name
+                                </div>
+                                <div className="text-[#334155] text-lg">
+                                    Lecturer
+                                </div>
+
+                                <div className="text-[#EAB308] mb-3 flex flex-row text-lg">
+                                    {[...Array(5)].map((_, i) => (
+                                        <StarIcon key={i} className="w-8" />
+                                    ))}
+                                    <div className="text-[#334155] font-bold">
+                                        (Rating)
+                                    </div>
+                                </div>
+                                <div className="mb-3 flex justify-between text-[#334155]">
+                                    <div>hours</div> <div>lectures</div>
+                                    <div> difficulity</div>
+                                </div>
+                                <div className="text-xl mb-3 font-bold">FEE</div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </>
     );
